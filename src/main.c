5a7455ad04f33a74dfb820a4ae16242172dc8158
Lucas Sacchi <rmlucas0874@gmail.com>
2024-11-16 14:44:52 -0300

    Removing unecessary comments 🔨

--- src/main.c
+++ src/main.c
@@ -7,20 +7,16 @@
 #include "progress.h"
 
 int main() {
-    srand(time(NULL));  // Inicializa o gerador de números aleatórios
+    srand(time(NULL));  
 
     pthread_t threads[NUM_THREADS];
 
-    // Inicializa os dados das threads
     init_threads(threads);
 
-    // Inicia a função que exibe o progresso da corrida
     mostrar_progresso();
 
-    // Espera todas as threads terminarem
     wait_for_threads(threads);
 
-    // Exibe o vencedor
     display_winner();
 
     return 0;
